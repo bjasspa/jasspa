197124c645b8ad99489668409912e321a1e5bb00
Jon Green <jon@jasspa.com>
2001-07-16 19:13:55 +0000

    CVS Page update
    git-svn-id: file:///media/bill/diskAshur2/backup/svn/microemacs/trunk@94 a259ec66-be10-11ea-b557-b34f8aa0b108

--- microemacs/doc/crtehf.bat
+++ microemacs/doc/crtehf.bat
@@ -1,17 +1,17 @@
 @echo off
 
-cd c:\me\nroff
+cd c:\me\doc
 nmake meehf.hts
 if %errorlevel% == 1 then goto exit
 
 cd c:\me\ehf
 del -y c:\me\ehf\me\*.*
-hts2html -l .htm ..\nroff\meehf.hts
+hts2html -l .htm ..\doc\meehf.hts
 if %errorlevel% == 1 then goto exit
 
 copy me.htm me\1.htm
 cd .\me
-me32 "@ehftools" *.htm
+"c:\Program Files\JASSPA\MicroEmacs\me32" "@ehftools" *.htm
 cd ..
 
 :exit
