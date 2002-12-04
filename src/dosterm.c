d17a0b1226db07c30ee464363d90eadff622011d
Jon Green <jon@jasspa.com>
2002-12-04 23:27:43 +0000

    Corrected the width setting
    git-svn-id: file:///media/bill/diskAshur2/backup/svn/microemacs/trunk/microemacs@237 a259ec66-be10-11ea-b557-b34f8aa0b108

--- src/dosterm.c
+++ src/dosterm.c
@@ -540,8 +540,8 @@ ibmChangeSRes(void)
         rg.h.bh = 0 ;		/* set screen page number */
         int86(0x10, &rg, &rg);
 #endif
-        frameChangeDepth(meTRUE,frameCur->depth+1-row);
-        frameChangeWidth(meTRUE,frameCur->width-col);
+        frameChangeDepth(meTRUE,row-(frameCur->depth+1));
+        frameChangeWidth(meTRUE,col-frameCur->width);
     }
     /* Stop that horrible blinking */
     rg.x.ax = 0x1003;		/* blink state dos call */
