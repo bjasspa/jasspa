5d15b692321a4498088d416cb17276bbe02d3469
Jon Green <jon@jasspa.com>
2002-12-08 15:49:20 +0000

    Changed the release date
    git-svn-id: file:///media/bill/diskAshur2/backup/svn/microemacs/trunk@246 a259ec66-be10-11ea-b557-b34f8aa0b108

--- microemacs/src/evers.h
+++ microemacs/src/evers.h
@@ -43,8 +43,8 @@
  * THE BUILD DATE OF THE SOFTWARE AND MUST NOT BE MODIFIED AS SUCH */
 #define meCENTURY       "20"          /* Current century. Y2 complient :-) */
 #define meYEAR          "02"          /* Current year */
-#define meMONTH         "04"          /* Current month */
-#define meDAY           "12"          /* Day of the month */
+#define meMONTH         "12"          /* Current month */
+#define meDAY           "05"          /* Day of the month */
 
 /* Version information - Date of build */
 #define	meDATE          meYEAR "/" meMONTH "/" meDAY 
