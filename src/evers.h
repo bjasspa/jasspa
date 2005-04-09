deb809a5c606e5c5df80032a2abf50d44aa47558
Jon Green <jon@jasspa.com>
2005-04-09 18:55:18 +0000

    Updated for RC2
    git-svn-id: file:///media/bill/diskAshur2/backup/svn/microemacs/trunk/microemacs@1193 a259ec66-be10-11ea-b557-b34f8aa0b108

--- src/evers.h
+++ src/evers.h
@@ -43,8 +43,8 @@
  * THE BUILD DATE OF THE SOFTWARE AND MUST NOT BE MODIFIED AS SUCH */
 #define meCENTURY       "20"          /* Current century. Y2 complient :-) */
 #define meYEAR          "05"          /* Current year */
-#define meMONTH         "02"          /* Current month */
-#define meDAY           "24"          /* Day of the month */
+#define meMONTH         "04"          /* Current month */
+#define meDAY           "09"          /* Day of the month */
 
 /* Version information - Date of build */
 #define	meDATE          meYEAR "/" meMONTH "/" meDAY 
