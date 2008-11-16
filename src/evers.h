92ba2810c711490e4b2844ae003eefc35cfee84c
Steven Phillips <bill@jasspa.com>
2008-11-16 22:28:58 +0000

    Upped the version to 2008-11-10
    git-svn-id: file:///media/bill/diskAshur2/backup/svn/microemacs/trunk/microemacs@2051 a259ec66-be10-11ea-b557-b34f8aa0b108

--- src/evers.h
+++ src/evers.h
@@ -42,9 +42,9 @@
  * PROBLEMS OCCUR. THESE VALUES ARE NOT INTENDED TO REPRESENT 
  * THE BUILD DATE OF THE SOFTWARE AND MUST NOT BE MODIFIED AS SUCH */
 #define meCENTURY       "20"          /* Current century. Y2 complient :-) */
-#define meYEAR          "06"          /* Current year */
+#define meYEAR          "08"          /* Current year */
 #define meMONTH         "11"          /* Current month */
-#define meDAY           "11"          /* Day of the month */
+#define meDAY           "10"          /* Day of the month */
 
 /* Version information - Date of build */
 #define	meDATE          meYEAR "/" meMONTH "/" meDAY 
