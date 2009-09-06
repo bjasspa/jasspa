0bb7531d7c510093210a4f9be11a1de59a72178c
Jon Green <jon@jasspa.com>
2009-09-06 20:00:48 +0000

    Updated for the 20090909 release
    git-svn-id: file:///media/bill/diskAshur2/backup/svn/microemacs/trunk/microemacs@2112 a259ec66-be10-11ea-b557-b34f8aa0b108

--- packages/hpux/opt_depot_10.sh
+++ packages/hpux/opt_depot_10.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
+#set -x
 #
 #  System        : MicroEmacs
 #  Module        : Package Build script.
 #  Object Name   : $RCSfile: opt_depot_10.sh,v $
-#  Revision      : $Revision: 1.9 $
-#  Date          : $Date: 2009-08-29 17:50:34 $
+#  Revision      : $Revision: 1.10 $
+#  Date          : $Date: 2009-09-06 20:00:48 $
 #  Author        : $Author: jon $
 #  Created By    : <unknown>
 #  Created       : Sun Aug 17 12:58:23 2003
-#  Last Modified : <090829.1805>
+#  Last Modified : <040328.0620>
 #
-# Rules to build the HPUX 10.20 depot file. We build the executable only.
+# Rules to build the HPUX 11.xx depot file. We build the executable only.
 #
 MKDIR=mkdir
-SEARCH_PATH="/opt/jasspa/company:/opt/jasspa/macros:/opt/jasspa/spelling"
 BASEDIR=.
 TOPDIR=../..
 VER_YEAR="09"
@@ -22,50 +22,86 @@ VER_DAY="09"
 VERSION="20${VER_YEAR}${VER_MONTH}${VER_DAY}"
 METREE=jasspa-metree-${VERSION}.tar.gz
 MESRC=jasspa-mesrc-${VERSION}.tar.gz
-MEBIN=jasspa-me-hpux-pa-10.20-${VERSION}.gz
-NEBIN=jasspa-ne-hpux-pa-10.20-${VERSION}.gz
+MEBIN=jasspa-me-hpux-pa-10.00-${VERSION}.gz
+NEBIN=jasspa-ne-hpux-pa-10.00-${VERSION}.gz
 BASEFILESET="${METREE} ${MEBIN}"
+#
+DOCFILESET="COPYING change.log faq.txt license.txt readme.txt jasspame.pdf"
+SPELLFILESET="enus"
+#SPELLFILESET="dede enus engb eses fifi frfr itit plpl ptpt ruye ruyo"
 # Set to "mak" for native or "gmk" for GCC
 PLATFORM=`uname`
 MAKEBAS=hpux10
 CCMAK=gmk
+DEPOTFILE=opt_depot_10.psf
+#
+JASSPACOM="www.jasspa.com"
+#JASSPACOM="194.164.217.96"
 #
 # Pull the files over from the release and source areas.
 #
 if [ ! -f ${METREE} ] ; then
-    if [ -f ${TOPDIR}/release/www/${METREE} ] ; then
-        cp ${TOPDIR}/release/www/${METREE} .
+     wget ${JASSPACOM}/release_${VERSION}/${METREE}
+    if [ ! -f ${METREE} ] ; then
+        echo "Cannot find file ${METREE}"
+        exit 1
     fi
 fi
+# Get the source
 if [ ! -f ${MESRC} ] ; then
-    if [ -f ${TOPDIR}/release/www/${MESRC} ] ; then
-        cp ${TOPDIR}/release/www/${MESRC} .
+     wget ${JASSPACOM}/release_${VERSION}/${MESRC}
+    if [ ! -f ${MESRC} ] ; then
+        echo "Cannot find file ${METREE}"
+        exit 1
     fi
 fi
+# Get the manual page
 if [ ! -f me.1 ] ; then
-    if [ -f ${TOPDIR}/release/www/me.1.gz ] ; then
-        gunzip -c ${TOPDIR}/release/www/me.1.gz > me.1
+     wget ${JASSPACOM}/release_${VERSION}/me.1
+    if [ ! -f me.1 ] ; then
+        echo "Cannot find file me.1"
+        exit 1
     fi
 fi
+# Build the binary
 if [ ! -f ${MEBIN} ] ; then
-    if [ -f ${TOPDIR}/src/${MEBIN} ] ; then
-        cp ${TOPDIR}/src/${MEBIN} ${MEBIN}
-    elif [ -f ${TOPDIR}/src/me ] ; then
-        gzip -9 -c ${TOPDIR}/src/me > ${MEBIN}
-    else
-        # Build me
-        gunzip -c ${MESRC} | tar xf -
-        (cd me${VER_YEAR}${VER_MONTH}${VER_DAY}/src; sh build -ne -m hpux10.gmk)
-        gzip -9 -c me${VER_YEAR}${VER_MONTH}${VER_DAY}/src/ne > ${NEBIN}
-        (cd me${VER_YEAR}${VER_MONTH}${VER_DAY}/src; sh build -m hpux10.gmk)
-        gzip -9 -c me${VER_YEAR}${VER_MONTH}${VER_DAY}/src/me > ${MEBIN}
-        rm -rf me${VER_YEAR}${VER_MONTH}${VER_DAY}
-    fi
+    # Build me
+    gunzip -c ${MESRC} | tar xf -
+    (cd me${VER_YEAR}${VER_MONTH}${VER_DAY}/src; sh build -ne)
+    gzip -9 -c me${VER_YEAR}${VER_MONTH}${VER_DAY}/src/ne > ${NEBIN}
+    (cd me${VER_YEAR}${VER_MONTH}${VER_DAY}/src; sh build)
+    gzip -9 -c me${VER_YEAR}${VER_MONTH}${VER_DAY}/src/me > ${MEBIN}
+    rm -rf me${VER_YEAR}${VER_MONTH}${VER_DAY}
 fi
+#
+# Get the document files
+#
+for FILE in ${DOCFILESET} ; do
+    if [ ! -f ${FILE} ] ; then
+        wget ${JASSPACOM}/release_${VERSION}/${FILE}
+    fi        
+    if [ ! -f ${FILE} ] ; then
+        echo "Cannot find file ${FILE}"
+        exit 1
+    fi
+done
+#
+# Get the spelling files
+#
+for FILE in ${SPELLFILESET} ; do
+    SPELLFILE="ls_${FILE}.tar.gz"
+    if [ ! -f ${SPELLFILE} ] ; then
+        wget ${JASSPACOM}/spelling/${SPELLFILE}
+    fi        
+    if [ ! -f ${SPELLFILE} ] ; then
+        echo "Cannot find file ${SPELLFILE}"
+        exit 1
+    fi
+done
 #
 # Test for the starting files.
 #
-for FILE in $BASEFILESET ; do
+for FILE in $BASEFILESET ${DOCFILESET} ; do
     if [ ! -f ${FILE} ] ; then
         echo "Cannot find file ${FILE}"
         exit 1
@@ -82,6 +118,25 @@ fi
 #
 gunzip -c ${METREE} | tar xf -
 #
+# Remove the windows specifics
+#
+rm -f ./jasspa/contrib/ME_4_all.reg
+rm -f ./jasspa/contrib/mesetup.reg
+#
+# Expand the documentation files
+#
+mkdir -p ./jasspa/doc
+for FILE in ${DOCFILESET} ; do
+    cp ${FILE} ./jasspa/doc
+done
+chmod -R go-w ./jasspa
+#
+# Unpack the spelling file
+for FILE in ${SPELLFILESET} ; do
+    SPELLFILE="ls_${FILE}.tar.gz"
+    gunzip -c ${SPELLFILE} | (cd ./jasspa/spelling; tar xvf - )
+done    
+#
 # Unpack the executable.
 #
 mkdir -p ${BASEDIR}/jasspa/bin
@@ -90,9 +145,43 @@ chmod a+rx ${BASEDIR}/jasspa/bin/me
 #
 # Build the man directory.
 #
-mkdir -p ${BASEDIR}/jasspa/man/cat1
-cp me.1 ${BASEDIR}/jasspa/man/cat1
-chmod a-wx ${BASEDIR}/jasspa/man/cat1/me.1
+mkdir -p ${BASEDIR}/jasspa/man/man1
+cp me.1 ${BASEDIR}/jasspa/man/man1
+chmod a-wx ${BASEDIR}/jasspa/man/man1/me.1
+#
+# Build the depot file
+#
+echo "vendor" > ${DEPOTFILE}
+echo "    tag jasspa" >> ${DEPOTFILE}
+echo "    description \"JASSPA MicroEmacs text editor www.jasspa.com\"" >> ${DEPOTFILE}
+echo "    title \"JASSPA MicroEmacs\"" >> ${DEPOTFILE}
+echo "end" >> ${DEPOTFILE}
+echo "product" >> ${DEPOTFILE}
+echo "    tag jasspa-me" >> ${DEPOTFILE}
+echo "    title \"JASSPA MicroEmacs\"" >> ${DEPOTFILE}
+echo "    description \"JASSPA MicroEmacs text editor www.jasspa.com\"" >> ${DEPOTFILE}
+echo "    # Revision, do not include a leading 0 on minor. i.e. YYYY.[M]MDD" >> ${DEPOTFILE}
+echo "    revision 2009.909" >> ${DEPOTFILE}
+echo "    #" >> ${DEPOTFILE}
+echo "    architecture S700/800_HPUX_10" >> ${DEPOTFILE}
+echo "    machine_type 9000/[78]*" >> ${DEPOTFILE}
+echo "    os_name HP-UX" >> ${DEPOTFILE}
+echo "    os_release ?.10.*" >> ${DEPOTFILE}
+echo "    os_version *" >> ${DEPOTFILE}
+echo "    copyright \"Copyright (c) 1988-2009 JASSPA - www.jasspa.com\"" >> ${DEPOTFILE}
+echo "    #" >> ${DEPOTFILE}
+echo "    fileset" >> ${DEPOTFILE}
+echo "        tag jasspa" >> ${DEPOTFILE}
+echo "        title \"JASSPA MicroEmacs\"" >> ${DEPOTFILE}
+echo "        revision 2009.909" >> ${DEPOTFILE}
+echo "        postinstall jasspame.postinstall" >> ${DEPOTFILE}
+echo "        postremove  jasspame.postremove" >> ${DEPOTFILE}
+echo "        #" >> ${DEPOTFILE}
+echo "        directory jasspa=/opt/jasspa" >> ${DEPOTFILE}
+echo "        file_permissions -u 022 -g bin -o bin" >> ${DEPOTFILE}
+echo "        file *" >> ${DEPOTFILE}
+echo "    end" >> ${DEPOTFILE}
+echo "end" >> ${DEPOTFILE}
 #
 # Build the ".prj" file.
 #
