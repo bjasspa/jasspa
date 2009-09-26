879e20b684226008ab61e12f400bfabe7826a961
Jon Green <jon@jasspa.com>
2009-09-26 17:48:17 +0000

    Reset the release date.
    git-svn-id: file:///media/bill/diskAshur2/backup/svn/microemacs/trunk/microemacs@2135 a259ec66-be10-11ea-b557-b34f8aa0b108

--- src/evers.h
+++ src/evers.h
@@ -44,7 +44,7 @@
 #define meCENTURY       "20"          /* Current century. Y2 complient :-) */
 #define meYEAR          "09"          /* Current year */
 #define meMONTH         "09"          /* Current month */
-#define meDAY           "09"          /* Day of the month */
+#define meDAY           "26"          /* Day of the month */
 
 /* Version information - Date of build */
 #define	meDATE          meYEAR "/" meMONTH "/" meDAY 
