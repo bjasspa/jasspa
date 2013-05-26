c79687467ad9edeb4cdd56bad5f867818e2211a3
Jon Green <jon@jasspa.com>
2013-05-26 12:23:33 +0000

    Revision of Sun package generation script
    git-svn-id: file:///media/bill/diskAshur2/backup/svn/microemacs/trunk@2269 a259ec66-be10-11ea-b557-b34f8aa0b108

--- microemacs/packages/solaris/sunpkg.sh
+++ microemacs/packages/solaris/sunpkg.sh
@@ -6,12 +6,17 @@ set -x
 MKDIR=mkdir
 DIRECTORIES="company doc icons macros spelling src bin"
 SEARCH_PATH="/opt/jasspa/company:/opt/jasspa/macros:/opt/jasspa/spelling"
-BASEDIR=.
+BASEDIR="fakeroot"
 TOPDIR=../..
 VER_YEAR="09"
 VER_MONTH="09"
 VER_DAY="09"
 VERSION="20${VER_YEAR}${VER_MONTH}${VER_DAY}"
+#
+LVER_YEAR="09"
+LVER_MONTH="10"
+LVER_DAY="11"
+LATEST="20${LVER_YEAR}${LVER_MONTH}${LVER_DAY}"
 # Processor type
 PROCESSOR=`uname -p`
 OSVERSION=`uname -r`
@@ -19,17 +24,17 @@ OSNAME=`uname -s`
 USERNAME=`/usr/xpg4/bin/id -u -n`
 GROUPNAME=`/usr/xpg4/bin/id -g -n`
 # Package name
-VERSTRING="20${VER_YEAR}.${VER_MONTH}.${VER_DAY}"
+VERSTRING="20${LVER_YEAR}.${LVER_MONTH}.${LVER_DAY}"
 PKGNAME=jasspa-me
-PKGBASENAME=${PKGNAME}-${OSNAME}${OSVERSION}-${PROCESSOR}-${VERSION}
+PKGBASENAME=${PKGNAME}-${OSNAME}${OSVERSION}-${PROCESSOR}-${LATEST}
 PKGFILENAME=${PKGBASENAME}.pkg.gz
 #
-MESRC=jasspa-mesrc-${VERSION}.tar.gz
-METREE=jasspa-metree-${VERSION}.tar.gz
-MEBIN=jasspa-me-${OSNAME}${OSVERSION}-${PROCESSOR}-${VERSION}.gz
-NEBIN=jasspa-ne-${OSNAME}${OSVERSION}-${PROCESSOR}-${VERSION}.gz
+MESRC=jasspa-mesrc-${LATEST}.tar.gz
+METREE=jasspa-metree-${LATEST}.tar.gz
+MEBIN=jasspa-me-${OSNAME}${OSVERSION}-${PROCESSOR}-${LATEST}.gz
+NEBIN=jasspa-ne-${OSNAME}${OSVERSION}-${PROCESSOR}-${LATEST}.gz
 BASEFILESET="${METREE} ${MEBIN} me.1"
-DOCFILESET="COPYING change.log faq.txt license.txt readme.txt jasspame.pdf"
+DOCFILESET="COPYING change.log faq.txt license.txt readme.txt jasspame.pdf jasspa-microemacs.desktop"
 #
 JASSPACOM="www.jasspa.com"
 #
@@ -47,11 +52,11 @@ fi
 if [ ! -f ${MEBIN} ] ; then
     # Build me
     gunzip -c ${MESRC} | tar xf -
-    (cd me${VER_YEAR}${VER_MONTH}${VER_DAY}/src; sh build -ne)
-    gzip -9 -c me${VER_YEAR}${VER_MONTH}${VER_DAY}/src/ne > ${NEBIN}
-    (cd me${VER_YEAR}${VER_MONTH}${VER_DAY}/src; sh build)
-    gzip -9 -c me${VER_YEAR}${VER_MONTH}${VER_DAY}/src/me > ${MEBIN}
-    rm -rf me${VER_YEAR}${VER_MONTH}${VER_DAY}
+    (cd me${LVER_YEAR}${LVER_MONTH}${LVER_DAY}/src; sh build -ne)
+    gzip -9 -c me${LVER_YEAR}${LVER_MONTH}${LVER_DAY}/src/ne > ${NEBIN}
+    (cd me${LVER_YEAR}${LVER_MONTH}${LVER_DAY}/src; sh build)
+    gzip -9 -c me${LVER_YEAR}${LVER_MONTH}${LVER_DAY}/src/me > ${MEBIN}
+    rm -rf me${LVER_YEAR}${LVER_MONTH}${LVER_DAY}
 fi
 #
 # Get the document files
@@ -78,66 +83,74 @@ if [ ! -f ${PKGFILENAME} ] ; then
     #
     # Unpack the tree
     #
+    mkdir -p ${BASEDIR}
+    mkdir -p ${BASEDIR}/opt
+    mkdir -p ${BASEDIR}/usr/share/applications
     gunzip -c ${METREE} | tar xf -
-    chmod -R go-w ${BASEDIR}/jasspa
+    mv jasspa ${BASEDIR}/opt
+    chmod -R go-w ${BASEDIR}/opt/jasspa
     #
     # Remove the windows specifics
     #
-    rm -f ${BASEDIR}/jasspa/contrib/ME_4_all.reg
-    rm -f ${BASEDIR}/jasspa/contrib/mesetup.reg
+    rm -f ${BASEDIR}/opt/jasspa/contrib/ME_4_all.reg
+    rm -f ${BASEDIR}/opt/jasspa/contrib/mesetup.reg
     #
     # Unpack the executable.
     #
-    mkdir -p ${BASEDIR}/jasspa/bin
-    gunzip -v -c ${MEBIN} > ${BASEDIR}/jasspa/bin/me
-    chmod a+rx ${BASEDIR}/jasspa/bin/me
-    mkdir -p ${BASEDIR}/jasspa/man/man1
-    cp me.1 ${BASEDIR}/jasspa/man/man1
-    chmod a-wx ${BASEDIR}/jasspa/man/man1/me.1
+    mkdir -p ${BASEDIR}/opt/jasspa/bin
+    gunzip -v -c ${MEBIN} > ${BASEDIR}/opt/jasspa/bin/me
+    chmod a+rx ${BASEDIR}/opt/jasspa/bin/me
+    mkdir -p ${BASEDIR}/opt/jasspa/man/man1
+    cp me.1 ${BASEDIR}/opt/jasspa/man/man1
+    chmod a-wx ${BASEDIR}/opt/jasspa/man/man1/me.1
+    #
+    # Build the desktop file
+    # 
+    cat jasspa-microemacs.desktop | sed -e "s#/usr/share#/opt#g" | sed -e "s#=me#=/opt/jasspa/bin/me#g" > ${BASEDIR}/usr/share/applications/jasspa-microemacs.desktop
+    chmod -R go-w ${BASEDIR}/usr
     #
     # Build the proto file
     #
-    rm -f prototype
-    rm -f pkginfo
-    echo "i pkginfo=./pkginfo" > prototype
-    pkgproto jasspa=/opt/jasspa | sed -e "s/${USERNAME} ${GROUPNAME}/root bin/g" >> prototype
+    rm -f ${BASEDIR}/prototype
+    rm -f ${BASEDIR}/pkginfo
+    echo "i pkginfo=./pkginfo" > ${BASEDIR}/prototype
+    (cd ${BASEDIR}; pkgproto usr=/usr opt=/opt | sed -e "s/${USERNAME} ${GROUPNAME}/root bin/g" >> prototype)
     #
     # Build the package info file
     #
-    echo 'PKG="'${PKGNAME}'"' > pkginfo
-    echo 'NAME="JASSPA MicroEmacs"' >> pkginfo
-    echo 'ARCH="'${PROCESSOR}'"' >> pkginfo
-    echo 'VERSION="'${VER_YEAR}.${VER_MONTH}.${VER_DAY}'"'  >> pkginfo
-    echo 'SUNW_PKGVERS="1.0"' >> pkginfo
-    echo 'CATEGORY="application"' >> pkginfo
-    echo 'VENDOR="www.jasspa.com"' >> pkginfo
-    echo 'EMAIL="support@jasspa.com"' >> pkginfo
-    echo 'PSTAMP="Jon Green"'  >> pkginfo
-    echo 'CLASSES="none"' >> pkginfo
+    echo 'PKG="'${PKGNAME}'"' > ${BASEDIR}/pkginfo
+    echo 'NAME="JASSPA MicroEmacs"' >> ${BASEDIR}/pkginfo
+    echo 'ARCH="'${PROCESSOR}'"' >> ${BASEDIR}/pkginfo
+    echo 'VERSION="'${LVER_YEAR}.${LVER_MONTH}.${LVER_DAY}'"'  >> ${BASEDIR}/pkginfo
+    echo 'SUNW_PKGVERS="1.0"' >> ${BASEDIR}/pkginfo
+    echo 'CATEGORY="application"' >> ${BASEDIR}/pkginfo
+    echo 'VENDOR="www.jasspa.com"' >> ${BASEDIR}/pkginfo
+    echo 'EMAIL="support@jasspa.com"' >> ${BASEDIR}/pkginfo
+    echo 'PSTAMP="Jon Green"'  >> ${BASEDIR}/pkginfo
+    echo 'CLASSES="none"' >> ${BASEDIR}/pkginfo
     #
     # Build the package.
     #
-    rm -rf ./${PKGNAME}
-    rm -f ${PKGFILENAME}
-    rm -f ${PKGNAME}pkg-sun-${PROCESSOR}-${OSVERSION}-${VERSION}.zip
-    /usr/bin/pkgmk -d . -f ./prototype ${PKGNAME}
-    /usr/bin/pkgtrans -o -s . ${PKGBASENAME}.pkg ${PKGNAME}
-    gzip -9 ${PKGBASENAME}.pkg
+    rm -rf ${BASEDIR}/${PKGNAME}
+    rm -f ${BASEDIR}/${PKGFILENAME}
+    rm -f ${BASEDIR}/${PKGNAME}pkg-sun-${PROCESSOR}-${OSVERSION}-${LATEST}.zip
+    (cd ${BASEDIR}; /usr/bin/pkgmk -d . -f ./prototype ${PKGNAME})
+    (cd ${BASEDIR}; /usr/bin/pkgtrans -o -s . ${PKGBASENAME}.pkg ${PKGNAME})
+    gzip -9 -c ${BASEDIR}/${PKGBASENAME}.pkg > ${PKGBASENAME}.pkg.gz
     #
     # Clean up
     #
-    rm -f prototype
-    rm -f pkginfo
-    rm -rf ./jasspa
+    rm -f ${BASEDIR}
     rm -rf ./${PKGNAME}
 fi        
+exit 0
 #
 # Build the spelling packages.
 #
 SPELLFILESET="dede enus engb eses fifi frfr itit plpl ptpt ruye ruyo"
 for FILE in ${SPELLFILESET} ; do
     # Name of the package
-    PKGSPELLFILE=jasspa-${FILE}-${OSNAME}-all-${VERSION}
+    PKGSPELLFILE=jasspa-${FILE}-${OSNAME}-all-${LATEST}
     if [ ! -f ${PKGSPELLFILE}.pkg.gz  ] ; then 
         # Get the name of the file.
         SPELLFILE="ls_${FILE}.tar.gz"
