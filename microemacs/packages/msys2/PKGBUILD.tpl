0ce28fd550823721f435bbbfdd286b392e1dcbfa
Detlef Groth <75636+mittelmark@users.noreply.github.com>
2024-09-11 16:39:00 +0200

    intial msys setup for pacman package

--- microemacs/packages/msys2/PKGBUILD.tpl
+++ microemacs/packages/msys2/PKGBUILD.tpl
@@ -7,20 +7,17 @@ pkgdesc="Jasspa MicroEmacs Text Editor"
 arch=('x86_64')
 url="https://github.com/bjasspa/jasspa"
 license=('GPL')
-source=("mec.exe" "mew.exe" "readme.txt" "COPYING.txt")
+source=("mecs.exe" "mews.exe" "readme.txt" "COPYING.txt")
 sha256sums=(
-    'digest-mec.exe'
-    'digest-mew.exe'
+    'digest-mecs.exe'
+    'digest-mews.exe'
     'digest-readme.txt'
     'digest-COPYING.txt')
-noextract=('macros/')
 
 options=("!strip")
 package() {
-   install -Dm755 "${srcdir}/mec.exe"     "${pkgdir}/usr/bin/mec.exe"
-   install -Dm755 "${srcdir}/mec.exe"     "${pkgdir}/usr/bin/mew.exe"   
+   install -Dm755 "${srcdir}/mecs.exe"    "${pkgdir}/usr/bin/mecs.exe"
+   install -Dm755 "${srcdir}/mews.exe"    "${pkgdir}/usr/bin/mews.exe"   
    install -Dm644 "${srcdir}/readme.txt"  "${pkgdir}/usr/share/jasspa/readme.txt"
    install -Dm644 "${srcdir}/COPYING.txt" "${pkgdir}/usr/share/jasspa/COPYING.txt"
-   install -d "${pkgdir}/usr/share/jasspa/macros"
-   cp -r "macros"  "${pkgdir}/usr/share/jasspa/"
 }
--- microemacs/packages/msys2/tpl2pkg.sh
+++ microemacs/packages/msys2/tpl2pkg.sh
@@ -9,8 +9,11 @@ function tpl2pkg {
       outfile=$(echo $2 | sed -E 's/.tpl$//')
       printf "$version $outfile\n"
       dld="https://github.com/bjasspa/jasspa/releases/download/me_${version}"
-      if [ ! -f "Jasspa_MicroEmacs_${version}_bin_windows_binaries.zip" ]; then
-         wget "${dld}/Jasspa_MicroEmacs_${version}_bin_windows_binaries.zip"
+      if [ ! -f "Jasspa_MicroEmacs_${version}_abin_windows_mecs.zip" ]; then
+         wget "${dld}/Jasspa_MicroEmacs_${version}_abin_windows_mecs.zip"
+      fi
+      if [ ! -f "Jasspa_MicroEmacs_${version}_abin_windows_mews.zip" ]; then
+         wget "${dld}/Jasspa_MicroEmacs_${version}_abin_windows_mews.zip"
       fi
       if [ ! -f "Jasspa_MicroEmacs_${version}_macros.zip" ]; then
          wget "${dld}/Jasspa_MicroEmacs_${version}_macros.zip"
@@ -22,14 +25,16 @@ function tpl2pkg {
           rm -rf microemacs-package
       fi
       mkdir microemacs-package
-      unzip -o Jasspa_MicroEmacs_${version}_bin_windows_binaries.zip -d microemacs-package
-      unzip -o Jasspa_MicroEmacs_${version}_macros.zip -d microemacs-package
-      unzip -o Jasspa_MicroEmacs_${version}_help_ehf.zip -d microemacs-package
-      mv microemacs-package/bin/*/me?.exe microemacs-package/
-      dig=$(sha256sum microemacs-package/mec.exe | cut -c 1-64)
-      sed -E "s/digest-mec.exe/${dig}/" $2 > microemacs-package/PKGBUILD
-      dig=$(sha256sum microemacs-package/mew.exe | cut -c 1-64)      
-      sed -i -E "s/digest-mew.exe/${dig}/" microemacs-package/PKGBUILD
+      unzip -o Jasspa_MicroEmacs_${version}_abin_windows_mecs.zip -d microemacs-package
+      unzip -o Jasspa_MicroEmacs_${version}_abin_windows_mews.zip -d microemacs-package
+      #mv microemacs-package/bin/*/*.exe microemacs-package/
+      #unzip -o Jasspa_MicroEmacs_${version}_macros.zip -d microemacs-package
+      #unzip -o Jasspa_MicroEmacs_${version}_help_ehf.zip -d microemacs-package
+      mv microemacs-package/bin/*/me?s.exe microemacs-package/
+      dig=$(sha256sum microemacs-package/mecs.exe | cut -c 1-64)
+      sed -E "s/digest-mecs.exe/${dig}/" $2 > microemacs-package/PKGBUILD
+      dig=$(sha256sum microemacs-package/mews.exe | cut -c 1-64)      
+      sed -i -E "s/digest-mews.exe/${dig}/" microemacs-package/PKGBUILD
       dig=$(sha256sum microemacs-package/readme.txt | cut -c 1-64)      
       sed -i -E "s/digest-readme.txt/${dig}/" microemacs-package/PKGBUILD
       dig=$(sha256sum microemacs-package/COPYING.txt | cut -c 1-64)      
