e61ed6b9ba2ae3274608b8f948dec953ff77a691
Detlef Groth <75636+mittelmark@users.noreply.github.com>
2025-03-10 09:23:00 +0100

    adding go example file

---
+++ microemacs/test/go/hello.go
@@ -0,0 +1,14 @@
+// -!- go -!- //////////////////////////////////////////////////////////////
+// program should be runnable for instance via
+// go run hello.go
+package main
+
+import "fmt"
+
+/* hello this is a very long comment which wraps two lines but has too many characters,
+ * much longer than it should be for a single line. It can be used to check the formatting of long comment lines.
+*/
+ 
+func main() {
+    fmt.Println("Hello world!")
+}
