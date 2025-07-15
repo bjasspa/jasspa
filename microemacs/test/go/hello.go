5be70bb3e08a798c4483a0df3683b54ddb66a406
Detlef Groth <75636+mittelmark@users.noreply.github.com>
2025-07-15 07:07:00 +0200

    initial file-tools cpp setup

--- microemacs/test/go/hello.go
+++ microemacs/test/go/hello.go
@@ -3,12 +3,13 @@
 // go run hello.go
 package main
 
-import "fmt"
+import 
+"fmt"
 
 /* hello this is a very long comment which wraps two lines but has too many characters,
  * much longer than it should be for a single line. It can be used to check the formatting of long comment lines.
-*/
- 
-func main() {
-    fmt.Println("Hello world!")
+ */
+
+func main () {
+ fmt.Printl("Hello world!")
 }
