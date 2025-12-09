e5eb5cdeb0478f0bbcac628a1ea2da01ad86d784
Steven Phillips <steve@asid.family>
2025-12-09 11:59:58 +0000

    Added window bat equivalent of cmdwait

---
+++ microemacs/test/emf/cmdwait.bat
@@ -0,0 +1,32 @@
+@echo off
+echo Here  0
+ping 127.0.0.1 -n 2 > nul
+echo Here  1
+ping 127.0.0.1 -n 2 > nul
+echo Here  2
+ping 127.0.0.1 -n 2 > nul
+echo Here  3
+ping 127.0.0.1 -n 2 > nul
+echo Here  4
+ping 127.0.0.1 -n 2 > nul
+echo Here  5
+ping 127.0.0.1 -n 2 > nul
+echo Here  6
+ping 127.0.0.1 -n 2 > nul
+echo Here  7
+ping 127.0.0.1 -n 2 > nul
+echo Here  8
+ping 127.0.0.1 -n 2 > nul
+echo Here  9
+ping 127.0.0.1 -n 2 > nul
+echo Here 10
+ping 127.0.0.1 -n 2 > nul
+echo Here 11
+ping 127.0.0.1 -n 2 > nul
+echo Here 12
+ping 127.0.0.1 -n 2 > nul
+echo Here 13
+ping 127.0.0.1 -n 2 > nul
+echo Here 14
+ping 127.0.0.1 -n 2 > nul
+echo Here END
